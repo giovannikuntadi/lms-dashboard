1baf685dc86a5ff555b2e802b3c3773f499cf1c0
giovannikuntadi <giovannikuntadi@gmail.com>
2026-08-28 14:35:11 +0700

    adjustment

--- src/components/Breadcrumb 2.tsx
+++
@@ -1,7 +0,0 @@
-interface BreadcrumbProps {
-  children: string;
-}
-
-export function Breadcrumb({ children }: BreadcrumbProps) {
-  return <span className="text-text-secondary text-sm font-normal">{children}</span>;
-}
