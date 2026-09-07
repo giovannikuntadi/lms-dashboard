b0e5f86a53ebd553b58147a57e089cd955fc7790
giovannikuntadi <giovannikuntadi@gmail.com>
2026-09-07 14:37:06 +0700

    github pages adjustment

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Navigate, Outlet, Route, Routes } from 'react-router';
+import { BrowserRouter, Navigate, Outlet, Route, Routes } from 'react-router';
 import { RequireGuest } from './routes/RequireGuest';
 import { RequireAuth } from './routes/RequireAuth';
 import { Login } from './pages/AuthPage/Login';
@@ -22,7 +22,7 @@ export function App() {
   }
 
   return (
-    <>
+    <BrowserRouter basename="/lms-dashboard">
       <Routes>
         <Route element={<RequireGuest />}>
           <Route path="/login" element={<Login />} />
@@ -49,6 +49,6 @@ export function App() {
           </Route>
         </Route>
       </Routes>
-    </>
+    </BrowserRouter>
   );
 }
